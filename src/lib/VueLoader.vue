84e09aba2f19824a8085bfed819f0c6698f8a5c9
Petr Ptacek <petr.ptacek@olc.cz>
2023-05-04 08:45:05 +0200

    update

--- src/lib/VueLoader.vue
+++ src/lib/VueLoader.vue
@@ -26,9 +26,15 @@ withDefaults(
     class="vue-loader"
     :class="{ 'vue-loader--hidden': !visible }"
   >
-    <div class="vue-loader__circle-1">
-      <div class="vue-loader__circle-2"></div>
+    <!--LOADER-->
+    <div class="vue-loader__loader">
+      <slot>
+        <div class="vue-loader__circle-1">
+          <div class="vue-loader__circle-2"></div>
+        </div>
+      </slot>
     </div>
+    <!--MESSAGE-->
     <div v-if="!disableMessage" class="vue-loader__message">
       <slot name="message" :message="message">
         {{ message }}
@@ -38,21 +44,33 @@ withDefaults(
 </template>
 
 <style>
-.vue-loader, .vue-loader * {
+/* Utility class to usage in the application, that use vue-loader */
+.vue-loader-container {
+  position: relative;
+}
+
+.vue-loader,
+.vue-loader * {
   padding: 0;
   margin: 0;
-  box-sizing: border-box;
+  box-sizing: inherit;
 }
 
 .vue-loader {
-  --spinner-circle1--color: #5591f5;
-  --spinner-circle1--size: 100px;
-  --spinner-circle1--duration: 2s;
-  --spinner-circle2--color: #27a769;
-  --spinner-circle2--size: 80px;
-  --spinner-circle2--duration: 1.5s;
+  /*CIRCLE 1*/
+  --vl-circle1--color: #5591f5;
+  --vl-circle1--size: 100px;
+  --vl-circle1--stroke-width: 11px;
+  --vl-circle1--duration: 2s;
+
+  /*CIRCLE 2*/
+  --vl-circle2--color: #27a769;
+  --vl-circle2--size: 80px;
+  --vl-circle2--stroke-width: 11px;
+  --vl-circle2--duration: 1.5s;
 
   position: absolute;
+  box-sizing: border-box;
   width: 100%;
   height: 100%;
   top: 0;
@@ -62,7 +80,13 @@ withDefaults(
   align-items: center;
   flex-direction: column;
   z-index: 10;
-  background-color: rgba(132, 132, 132, 0.35);
+  font-size: 10px;
+
+
+  /* From https://css.glass */
+  background: rgba(255, 255, 255, 0.3);
+  //border-radius: 16px;
+  box-shadow: inset 0 4px 30px rgba(0, 0, 0, 0.1);
   backdrop-filter: blur(2px);
 }
 
@@ -71,15 +95,14 @@ withDefaults(
 }
 
 .vue-loader__message {
-  margin-top: 15px;
+  font-size: 2.2em;
+  margin-top: 0.5em;
   font-weight: 500;
-  font-size: 2.2rem;
   font-family: sans-serif;
   display: inline-block;
   -webkit-text-fill-color: transparent;
   -webkit-background-clip: text;
-  background-image: linear-gradient(120deg, var(--spinner-circle1--color), var(--spinner-circle2--color));
-  color: #fff;
+  background-image: linear-gradient(120deg, var(--vl-circle1--color), var(--vl-circle2--color));
 }
 
 .vue-loader__circle-1,
@@ -94,17 +117,17 @@ withDefaults(
   display: flex;
   align-items: center;
   justify-content: center;
-  width: var(--spinner-circle1--size);
-  height: var(--spinner-circle1--size);
-  border-left: 12px solid var(--spinner-circle1--color);
-  animation-duration: var(--spinner-circle1--duration);
+  width: var(--vl-circle1--size);
+  height: var(--vl-circle1--size);
+  border-left: var(--vl-circle1--stroke-width) solid var(--vl-circle1--color);
+  animation-duration: var(--vl-circle1--duration);
 }
 
 .vue-loader__circle-2 {
-  width: var(--spinner-circle2--size);
-  height: var(--spinner-circle2--size);
-  border-right: 12px solid var(--spinner-circle2--color);
-  animation-duration: var(--spinner-circle2--duration);
+  width: var(--vl-circle2--size);
+  height: var(--vl-circle2--size);
+  border-right: var(--vl-circle1--stroke-width) solid var(--vl-circle2--color);
+  animation-duration: var(--vl-circle2--duration);
 }
 
 @keyframes spinner {
--- src/sample/App.vue
+++ src/sample/App.vue
@@ -9,20 +9,77 @@ export default defineComponent({
 
 <script setup lang="ts">
 import { VueLoader } from "@/lib";
+import { ref }       from "vue";
 
-interface Props {
-}
-
-interface Emits {
-}
-
-const props = defineProps<Props>();
-const emit = defineEmits<Emits>();
+const loaderVisible = ref(false);
 </script>
 
 <template>
-  <div>
-    <div style="width: 500px"></div>
+  <div class="container">
+    <button @click="loaderVisible = !loaderVisible">Toggle Loader</button>
+
+    <div class="vue-loader-container" style="width: 500px; height: 500px;">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci ducimus ea incidunt laudantium non nulla
+        odio placeat quam quia reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci ducimus ea incidunt laudantium non nulla
+        odio placeat quam quia reprehenderit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci ducimus ea incidunt laudantium non nulla
+        odio placeat quam quia reprehenderit.
+      </p>
+
+      <VueLoader :visible="loaderVisible" message="Loading ..." />
+    </div>
   </div>
-  <VueLoader visible message="Loading ..." />
 </template>
+
+<style>
+* {
+  box-sizing: inherit;
+  padding: 0;
+  margin: 0;
+}
+
+html {
+  color: #212121;
+}
+
+body {
+  box-sizing: border-box;
+}
+
+p {
+  margin-bottom: 15px;
+}
+
+button {
+  padding: 10px;
+  text-transform: uppercase;
+  border: 2px solid #494747;
+  background-color: #ad49de;
+  cursor: pointer;
+}
+
+.container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  background-color: #dcd8d8;
+}
+
+.vue-loader-container {
+  border-radius: 20px;
+  overflow: hidden;
+  border: 4px solid #494747;
+  line-height: 1.5;
+  padding: 25px;
+  background-color: #ad49de;
+}
+</style>
